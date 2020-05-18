129fc25e362076175d494056dd1905e839a9c839
sushaoci <sushaociqiqi@gmail.com>
2020-05-18 21:28:28 +0800

    fsdf

--- js/render.js
+++ js/render.js
@@ -4,7 +4,7 @@ function renderPD(list) {
 
     const fragment = document.createDocumentFragment();
 
-    for(let i = 0; i < list.data.length; i++) {
+    for (let i = 0; i < list.data.length; i++) {
 
         const product = document.createElement("div");
         product.className = "product";
@@ -28,16 +28,16 @@ function renderPD(list) {
         product.appendChild(title);
         product.appendChild(price);
 
-    
+
         fragment.appendChild(product);
     }
-    
+
     row.appendChild(fragment);
 }
 
 // 单一商品页
 function renderDetails(list) {
-    
+
     const left = document.querySelector('#left');
     const leftImg = document.createElement('img');
     leftImg.src = list.data.mainImg;
@@ -65,4 +65,94 @@ function renderDetails(list) {
     const madeIn = list.data.place;
     const prodPlace = document.querySelector('#prodPlace');
     prodPlace.innerHTML = '素材產地 / ' + madeIn + '<br>加工產地 / ' + madeIn;
-} 
+}
+
+// 购物车页面 
+function renderCartList(list) {
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < list.length; i++) {
+        const cartItem = document.createElement('div');
+        cartItem.classList = "cartItem";
+        cartItem.dataset.confirm = list[i].confirm;
+
+        const cartLeft = document.createElement('div');
+        cartLeft.classList = "cartLeft";
+
+        const cartRight = document.createElement('div');
+        cartRight.classList = "cartRight";
+
+        cartItem.appendChild(cartLeft);
+        cartItem.appendChild(cartRight);
+
+        const cartImgWrap = document.createElement('div');
+        cartImgWrap.classList = "cartImg";
+
+        const cartImg = document.createElement('img');
+        cartImg.src = list[i].mainImg;
+
+        cartImgWrap.appendChild(cartImg);
+        cartLeft.appendChild(cartImgWrap);
+
+        const cartBasic = document.createElement('div');
+        cartBasic.classList = "cartBasic";
+        cartBasic.innerHTML = list[i].name + '<br><br>' + list[i].id;
+
+        cartLeft.appendChild(cartBasic);
+
+        const cartQty = document.createElement('div');
+        cartQty.classList = "cartQty";
+
+        const cartM1 = document.createElement('div');
+        cartM1.classList = "cartMoji";
+        cartM1.textContent = "數量";
+
+        const qtySelect = document.createElement('select');
+        qtySelect.dataset.confirm = list[i].confirm;
+        // record(list[i].id, list[i].color.code, list[i].size, list[i].qty, qtySelect);
+
+        cartQty.appendChild(cartM1);
+        cartQty.appendChild(qtySelect);
+
+        const cartPiece = document.createElement('div');
+
+        const cartM2 = document.createElement('div');
+        cartM2.classList = "cartMoji";
+        cartM2.textContent = "單價";
+
+        const cartPrice = document.createElement('p');
+        cartPrice.id = "cartPiecePrice";
+        cartPrice.textContent = "￥" + list[i].price;
+
+        cartPiece.appendChild(cartM2);
+        cartPiece.appendChild(cartPrice);
+
+        const cartSub = document.createElement('div');
+
+        const cartM3 = document.createElement('div');
+        cartM3.classList = "cartMoji";
+        cartM3.textContent = "小計";
+
+        const cartTotal = document.createElement('p');
+        cartTotal.id = "cartSubTotal";
+        cartTotal.dataset.confirm = list[i].confirm;
+        cartTotal.textContent = "NT." + list[i].price * list[i].qty;
+
+        cartSub.appendChild(cartM3);
+        cartSub.appendChild(cartTotal);
+
+        cartRight.appendChild(cartQty);
+        cartRight.appendChild(cartPiece);
+        cartRight.appendChild(cartSub);
+
+        const cartDelete = document.createElement('div');
+        cartDelete.classList = "cartDelete";
+        cartDelete.dataset.confirm = list[i].confirm;
+        cartRight.appendChild(cartDelete);
+
+        fragment.appendChild(cartItem);
+    }
+
+    cartList.appendChild(fragment);
+
+}
